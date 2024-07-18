bab8131a6d15985e431b49f50531c3a9f53ffd15
Frank Manno <frankmanno@gmail.com>
2024-07-18 00:48:03 +0000

    initial movie controller and view

---
+++ app/controllers/movie.php
@@ -0,0 +1,31 @@
+<?php
+
+class Movie extends Controller {
+
+    public function index() {
+	    $this->view('movie/index');
+    }
+
+    public function search() {
+      if (!isset($_REQUEST['movie'])) {
+        // redirect to /movie
+        
+      }
+
+      $api = $this->model('Api');
+
+      $title = $_REQUEST['movie'];
+      $movie = $api->search_movie($title);
+
+      // $review = $api->get_movie_review($title);
+
+      echo '<pre>';
+      print_r($movie);
+      // print_r($review);
+      die;
+    }
+
+    public function add_rating($title = '', $rating = '') {
+      
+    }
+}
