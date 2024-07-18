47a91ea4fd68f2ec2e60804b6ab068b6dbcc3e90
Frank Manno <frankmanno@gmail.com>
2024-07-18 00:47:34 +0000

    added non-auth header

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -12,7 +12,7 @@ $is_admin = $_SESSION['is_admin'];
       <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
       <link href="/public/css/reports.css" rel="stylesheet">
         <link rel="icon" href="/favicon.png">
-        <title>COSC 4806</title>
+        <title>Ripened Tomatoes</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width">
         <meta name="apple-mobile-web-app-capable" content="yes">
@@ -24,7 +24,7 @@ $is_admin = $_SESSION['is_admin'];
   <div class="container-fluid">
     <a class="navbar-brand" href="/home">
       <img src="/public/img/reminders-logo.png" alt="COSC 4806" height="64" class="rounded">
-      COSC 4806
+      Ripened Tomatoes
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
---
+++ app/views/templates/headerNonAuth.php
@@ -0,0 +1,11 @@
+    <!DOCTYPE html>
+    <html lang="en">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="icon" href="/favicon.png">
+        <title>Ripened Tomatoes</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+    </head>
+    <body class="py-4 bg-light">
