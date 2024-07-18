1764ddb86e0bed50941e4245d522ad2b081b0ccb
Frank Manno <frankmanno@gmail.com>
2024-07-18 22:48:16 +0000

    redirect to movie

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,17 +1,3 @@
-<?php require_once 'app/views/templates/header.php' ?>
-<div class="container-fluid">
-    <div class="page-header pt-3" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>Welcome back, <?= $_SESSION['username'] ?>!</h1>
-                <p class="lead"> <?= date("F jS, Y"); ?></p>
-            </div>
-        </div>
-    </div>
+<?php
 
-    <div class="row">
-        <div class="col-lg-12">
-        </div>
-    </div>
-
-    <?php require_once 'app/views/templates/footer.php' ?>
+header('Location: /movie');
