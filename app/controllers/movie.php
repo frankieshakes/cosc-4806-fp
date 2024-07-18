52b9b143f1a24aafb5013c69eceb4cc9f0b80e1b
Frank Manno <frankmanno@gmail.com>
2024-07-18 22:57:04 +0000

    prelim search results page + test code for adding a rating

--- app/controllers/movie.php
+++ app/controllers/movie.php
@@ -2,33 +2,52 @@
 
 class Movie extends Controller {
 
-    public function index() {
-	    $this->view('movie/index');
-    }
+  private MovieReview $movieReview;
 
-    public function search() {
-      if (!isset($_REQUEST['movie'])) {
-        // redirect to /movie
-        
-      }
+  function __construct() {
+    $this->movieReview = $this->model('MovieReview');
+  }
 
-      $title = $_REQUEST['movie'];
+  public function index() {
+    $this->view('movie/index');
+  }
 
-      $api = $this->model('Api');
-
-      $movie = $api->search_movie($title);
-      $review = $api->get_movie_review($title);
-
-      echo '<pre>';
-      print_r($movie);
-      echo '</pre>';
-      echo '<pre>';
-      print_r($review);
-      echo '</pre>';
+  public function search() {
+    if (!isset($_REQUEST['movie'])) {
+      // redirect to /movie
+      header('Location: /movie');
       die;
     }
 
-    public function add_rating($title = '', $rating = '') {
-      
+    $title = $_REQUEST['movie'];
+
+    $api = $this->model('Api');
+
+    $movie = $api->search_movie($title);
+    $review = $api->get_movie_review($title);
+
+    $this->view('movie/results', ['title' => $title, 'movie' => $movie, 'review' => $review]);
+
+
+    // echo '<pre>';
+    // print_r($movie);
+    // echo '</pre>';
+    // echo '<pre>';
+    // print_r($review);
+    // echo '</pre>';
+    // die;
+  }
+
+  public function rate($title = '', $rating = '') {
+    echo '<pre>';
+    echo $title . " / " . $rating;
+    echo '<br />';      
+    $rating = intval($rating);
+    if ($rating > 0 && $rating <= 5) {
+      echo "movie rated: " . $rating;
+    } else {
+      echo "invalid rating";
     }
+    die;
+  }
 }
