75dff2d36683652aa71adf0c66964be33dcc81c2
Frank Manno <frankmanno@gmail.com>
2024-07-21 22:20:40 +0000

    about page + controller

---
+++ app/views/about/index.php
@@ -0,0 +1,21 @@
+<?php require_once 'app/views/templates/headerNonAuth.php' ?>
+<div class="container-fluid pb-5">
+
+  <div class="row d-flex justify-content-center align-items-center h-100 pt-3">
+    <div class="col col-lg-6">
+      <img src="/public/img/logo.png" alt="Ripened Tomatoes logo" height="256" class="rounded float-start me-4">
+      
+      <p>Welcome to Ripened Tomatoes, where the reviews are fresh and the scores are always juicy! Forget the spoil; here, every review is picked right off the vine of cinematic greatness.</p>
+
+      <p>Founded by a group of film enthusiasts who believed that movies, like fine wine, only get better with age, Ripened Tomatoes is your go-to source for reviews that have matured beautifully into ripe insights. Here, we celebrate the cream of the crop, the pick of the patch, and the gems that have stood the test of time.</p>
+
+      <p>At Ripened Tomatoes, we take a fresh approach to movie ratings. Our unique Freshness Meter isn’t just a gauge of how a film is received; it’s a celebration of films that have blossomed into their full potential. From blockbusters that ripened into beloved classics to indie films that slowly fermented into underground sensations, we cover them all.</p>
+
+      <p>Why settle for reviews that leave a sour taste in your mouth? Join us at Ripened Tomatoes, where we savor the flavor of great cinema and share reviews that are as flavorful as a perfectly ripe tomato. Grab a vine, hang out, and let’s get to the good stuff because here, every movie has its day in the sun!</p>
+
+      <p>So, whether you're looking for a film that’s sweet, tart, or perfectly balanced, Ripened Tomatoes has a little something for everyone's palate. Remember, life’s too short for rotten reviews!</p>
+    </div>
+  </div>
+</div>
+
+<?php require_once 'app/views/templates/footer.php' ?>
